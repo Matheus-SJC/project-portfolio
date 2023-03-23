6a709b588d0d6e1d230228df31f8c73b378a5617
Matheus Silverio <matheus.silverio132@gmail.com>
2023-03-23 00:51:51 -0300

    ajustes-1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,12 +2,19 @@ const darkTheme = document.querySelector('.dark-theme')
 const lightTheme = document.querySelector('.light-theme')
 const allTheme = document.querySelector('.all-theme')
 
-darkTheme.addEventListener('click', function() {
-    allTheme.classList.remove('light')
-    allTheme.classList.add('dark')
-})
+function switchBtn() {
+    if (darkTheme.style.display === 'none') {
+        darkTheme.style.display = 'block'
+        lightTheme.style.display = 'none'
+        allTheme.classList.remove('dark')
+        allTheme.classList.add('light')
+    } else {
+        darkTheme.style.display = 'none'
+        lightTheme.style.display = 'block'
+        allTheme.classList.remove('light')
+        allTheme.classList.add('dark')
+    }
+}
 
-lightTheme.addEventListener('click', function() {
-    allTheme.classList.remove('dark')
-    allTheme.classList.add('light')
-})
+darkTheme.addEventListener('click', switchBtn)
+lightTheme.addEventListener('click', switchBtn)
